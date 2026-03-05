77479594fbee61e0b641eb17c239714a7096fc28
Bedayatona <bedayatona@gmail.com>
2026-03-05 23:33:37 +0200

    fix: override Prisma connection limit to 10 in prisma.ts to prevent timeouts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const url = process.env.DATABASE_URL?.replace('connection_limit=1', 'connection_limit=10')
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: url
+    }
+  }
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
